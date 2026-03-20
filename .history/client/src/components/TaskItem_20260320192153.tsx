a6ba3571cce40fcd74bb43e3b11be49d26ff0fcd
Reno-mathews <renomathews10pro@gmail.com>
2026-03-20 19:59:20 +0530

    performing fixes

--- .history/client/src/components/TaskItem_20260320192153.tsx
+++ .history/client/src/components/TaskItem_20260320192153.tsx
@@ -1,5 +1,5 @@
 import API from "../api";
-import type { Task } from "../types/Task";
+import { Task } from "../types/Task";
 
 export default function TaskItem({ task, fetchTasks }: any) {
     const toggleTask = async () => {
